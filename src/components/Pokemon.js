c8f3cef09e3d86ef370c3f556c1192f871cca639
Jose <josemanki@gmail.com>
2021-07-09 23:18:20 +0200

    Capitalizing names

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -6,7 +6,7 @@ export default function Pokemon(props) {
             <div className="pokemon__card">
                 <h3>{`#${props.id}`}</h3>
                 <img className="pokemon__sprite" src={props.image} alt="Chansey" />
-                <h3>{props.name}</h3>
+                <h3 className="pokemon__name">{props.name}</h3>
                 <div className="pokemon__card__elements">
                     <img src="https://via.placeholder.com/30" alt="Normal" />
                     <img src="https://via.placeholder.com/30" alt="Normal" />
